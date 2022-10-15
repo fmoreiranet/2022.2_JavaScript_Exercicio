cc069f25e75532d81f908b7447060ad92b1efc9e
fMoreira <fabianomoreira.net@gmail.com.br>
2022-10-14 22:09:03 -0300

    forca

---
+++ extras/forca/js/forca.js
@@ -0,0 +1,27 @@
+
+(function () {
+    var palavra = sorteioPalavra();
+    var letras = contarLetraPalavra(palavra);
+    montarletrasTela(letras);
+})();
+
+function sorteioPalavra() {
+    var palavras = ["casa", "java", "números", "aluno", "constituição", "vai"]
+    var index = Math.floor(Math.random() * palavras.length);
+    return palavras[index];
+}
+
+function contarLetraPalavra(palavra = "") {
+    var contLetras = palavra.length;
+    return contLetras;
+}
+
+// ----------------------------------------------
+
+function montarletrasTela(quantLetras = 0) {
+    var letras = "";
+    for (var i = 1; i <= quantLetras; i++) {
+        letras += "<div> _ <div>";
+    }
+    document.getElementById("palavra_sorteada").innerHTML = letras;
+}
