6e7a5eddfdac2bac5bde6608e26017166c5199ac
fMoreira <fabianomoreira.net@gmail.com.br>
2022-10-26 21:57:56 -0300

    modificado

--- repeticao/js/repeticao_74.js
+++ repeticao/js/repeticao_74.js
@@ -31,7 +31,7 @@ function controllerNumeros() {
     saidaResultado(template);
 
     document.querySelectorAll("p").forEach(paragrafo => {
-        paragrafo.addEventListener("click", function (event) {
+        paragrafo.addEventListener("click", (event) => {
             console.log(event);
         })
     })
